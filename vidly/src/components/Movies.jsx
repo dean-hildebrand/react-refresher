5262c9bed6f6cf5b6c2a3540df4179b1a2ae4a3f
Dean Hildebrand <dean.hildebrand88@gmail.com>
2021-05-07 12:50:46 -0400

    rendered movies in table with attributes and delete button

---
+++ vidly/src/components/Movies.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import { getMovies } from "../services/fakeMovieService";
+
+export default class Movies extends Component {
+  state = {
+    movies: getMovies(),
+  };
+
+  render() {
+    // console.log(this.state.movies);
+    return (
+      <div>
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Title</th>
+              <th scope="col">Genre</th>
+              <th scope="col">Stock</th>
+              <th scope="col">Rate</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.movies.map((movie) => (
+              <tr>
+                <td>{movie.title}</td>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+                <button type="button" class="btn btn-danger">
+                  Delete
+                </button>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
